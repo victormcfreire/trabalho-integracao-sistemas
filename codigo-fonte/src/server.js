438da0b90f5903e35c77e20bf23e366d3976f5c8
Victor Martins <victor_martins_castro@hotmail.com>
2025-10-27 15:40:59 -0300

    inutilizando server.js

--- codigo-fonte/src/server.js
+++ codigo-fonte/src/server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando em http://localhost:${PORT}`);
+// });
