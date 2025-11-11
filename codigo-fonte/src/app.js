2b04d23df711219cd79df719c4f19711ae209f0a
Victor Martins <victor_martins_castro@hotmail.com>
2025-11-11 08:18:46 -0300

    testando nova config vercel

--- codigo-fonte/src/app.js
+++ codigo-fonte/src/app.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 // Rotas da API
-// app.use("/", routes);
+app.use("/", routes);
 
-// // Documentação Swagger
-// app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Documentação Swagger
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Adiciona a rota /error antes dos testes para garantir que ela existe
 app.get("/error", (req, res) => {
@@ -26,14 +26,14 @@ app.get("/teste", (req, res) => {
 });
 
 // Middleware 404 para rotas inexistentes
-// app.use((req, res) => {
-//   res.status(404).json({ error: "Rota não encontrada" });
-// });
+app.use((req, res) => {
+  res.status(404).json({ error: "Rota não encontrada" });
+});
 
-// // Middleware de erro genérico (evita que erros quebrem a aplicação)
-// app.use((err, req, res, next) => {
-//   console.error("Erro interno:", err);
-//   res.status(500).json({ error: "Erro interno do servidor" });
-// });
+// Middleware de erro genérico (evita que erros quebrem a aplicação)
+app.use((err, req, res, next) => {
+  console.error("Erro interno:", err);
+  res.status(500).json({ error: "Erro interno do servidor" });
+});
 
 export default app;
